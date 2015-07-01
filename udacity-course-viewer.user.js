8af87e3085adfb4358c4cee2300633de227d858b
Brad Erickson <eosrei@gmail.com>
2015-07-01 00:41:57 -0700

    Update class name to change reading area text only, not quizes

--- udacity-course-viewer.user.js
+++ udacity-course-viewer.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Udacity Full Page Course Viewer
-// @version      0.1
+// @version      0.2
 // @description  Udacity course content viewer displays text content in a 435px tall viewport div. This removes the viewport and displays the content at full height.
 // @oujs:author  eosrei
 // @match        https://www.udacity.com/course/viewer
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(".scale-media>div { position: relative;}");
+GM_addStyle(".scale-media>div.reading-area { position: relative;}");
 
