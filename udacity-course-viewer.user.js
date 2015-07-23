a6024ffa488ef4feb3e2d050b786fac3fdf18fcb
Brad Erickson <eosrei@gmail.com>
2015-07-23 10:21:42 -0700

    Fixed blank space below content.
    Added a negative bottom margin to .reading-area to counteract
    the positive bottom margin on .scale-media. The positive bottom
    margin cannot be removed since other content display depend on it.

--- udacity-course-viewer.user.js
+++ udacity-course-viewer.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Udacity Full Page Course Viewer
-// @version      0.2
+// @version      0.3
 // @description  Udacity course content viewer displays text content in a 435px tall viewport div. This removes the viewport and displays the content at full height.
 // @oujs:author  eosrei
 // @match        https://www.udacity.com/course/viewer
+// @homepage     https://github.com/eosrei/userscript-udacity-course-viewer
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(".scale-media>div.reading-area { position: relative;}");
+GM_addStyle(".scale-media>div.reading-area { position: relative; margin-bottom: -56.25%;}");
 
