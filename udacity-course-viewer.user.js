32cbed318d0d87750f66327f548affe760f0be9e
Brad Erickson <eosrei@gmail.com>
2015-07-27 23:07:40 -0700

    fix: Support GreaseMonkey in Firefox
    Greasemonkey runs in a special sandbox environment which blocks access to the
    user script code from the content script code. This means direct replacement
    of functions/variables provided by the content scripts is not possible.
    
    The quick less secure solution is unsafeWindow:
    http://wiki.greasespot.net/UnsafeWindow
    The secure solution (used) is Content Script Injection:
    http://wiki.greasespot.net/Content_Script_Injection
    
    Content Script Injection uses eval() to run a function in the content page
    scope.

--- udacity-course-viewer.user.js
+++ udacity-course-viewer.user.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         Udacity Full Page Course Viewer
-// @version      0.4
+// @namespace    https://github.com/eosrei/userscript-udacity-course-viewer
+// @version      0.5
 // @description  Makes full page text content in the Udacity Course Viewer instead of the original 435px tall scrolling div. Moves Downloadables to main sidebar, applies HighlightJS to example code.
 // @oujs:author  eosrei
-// @match        https://www.udacity.com/course/viewer
+// @author       eosrei
+// @copyright    2015+, eosrei (http://eosrei.net)
+// @license      MIT
+// @match        https://www.udacity.com/course/viewer*
 // @homepage     https://github.com/eosrei/userscript-udacity-course-viewer
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -14,29 +18,39 @@ GM_addStyle(".scale-media>div.reading-area { position: relative; margin-bottom: 
 // Disable word wrap for example code blocks and reduce fontsize to increase code visibility.
 GM_addStyle("pre { word-wrap: normal; font-size: 11px; }");
 
-// Add HighlightJS CSS/JS for code syntax highlighting.
-// Change to whatever highlight style CSS you want: https://github.com/isagalaev/highlight.js/tree/master/src/styles
-$("<link/>", {
-    rel: "stylesheet",
-    type: "text/css",
-    href: "//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/github-gist.min.css"
-}).appendTo("head");
-$.getScript('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js');
+// Make column generally used for Instructor Notes fill the available width because Downloadables is moving.
+GM_addStyle(".col-xs-8 { width: 100%;}");
+// Make Downloadables title match Get Help formatting.
+GM_addStyle("div.guided-tour-exhibit-downloadables-introduction h2 {color: #303030; font-size: 18px;}");
 
-// AngularJS to hijack the viewer's loadingComplete so we can run some code whenever the content is changed.
-var viewerControllerElement = document.querySelector('div.viewer-player');
-var viewerControllerScope = angular.element(viewerControllerElement).scope();
-// Make a copy of the original loadingComplete.
-var oldLoadingComplete = viewerControllerScope.loadingComplete;
-// Create a new loadingComplete
-viewerControllerScope.loadingComplete = function(c) {
-    // Run the original code.
-    oldLoadingComplete(c);
-    //console.log("LoadingComplete");
+// Code that needs to run in the Content Scope
+function contentScope() {
+  // Due to script execution order, we need to be sure everything is ready.
+  angular.element(document).ready(function () {
+    // Add HighlightJS CSS/JS for code syntax highlighting.
+    // Change to whatever highlight style CSS you want: https://github.com/isagalaev/highlight.js/tree/master/src/styles
+    $("<link/>", {
+      rel: "stylesheet",
+      type: "text/css",
+      href: "//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/github-gist.min.css"
+    }).appendTo("head");
+    $.getScript('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js');
 
-    // SetTimeout is non-ideal, but it's good enough to do what we need.
-    // @todo: Locate a better function so we don't need to have a timeout.
-    setTimeout(function(){
+    // AngularJS to hijack the viewer's loadingComplete so we can run some code whenever the content is changed.
+    var viewerControllerElement = document.querySelector('div.viewer-player');
+    var viewerControllerScope = angular.element(viewerControllerElement).scope();
+    // Make a copy of the original loadingComplete.
+    viewerControllerScope.oldLoadingComplete = viewerControllerScope.loadingComplete;
+    // Create a new loadingComplete
+    viewerControllerScope.loadingComplete = function(c) {
+      // Run the original code.
+      viewerControllerScope.oldLoadingComplete(c);
+      //console.log("LoadingComplete");
+
+      // Surprisingly loadingComplete() isn't called when loading is complete.
+      // SetTimeout is non-ideal, but it's good enough to do what we need.
+      // @todo: Locate a better function so we don't need to have a timeout.
+      setTimeout(function(){
         // Move Downloadables to the main sidebar under the Get Help section.
         $('div[data-supplemental-materials-list]').insertAfter('div[data-viewer-feedback]');
         //console.log("Moved Downloads");
@@ -46,10 +60,32 @@ viewerControllerScope.loadingComplete = function(c) {
             //console.log("Code block found");
             hljs.highlightBlock(block);
         });
-    }, 100);
+      }, 100);
+    };
+  });
 };
 
-// Make column generally used for Instructor Notes fill the available width because Downloadables is moving.
-GM_addStyle(".col-xs-8 { width: 100%;}");
-// Make Downloadables title match Get Help formatting.
-GM_addStyle("div.guided-tour-exhibit-downloadables-introduction h2 {color: #303030; font-size: 18px;}");
+// Content Script Injection (required for FF)
+// @src: http://wiki.greasespot.net/Content_Script_Injection
+function contentEval(source) {
+  // Check for function input.
+  if ('function' == typeof source) {
+    // Execute this function with no arguments, by adding parentheses.
+    // One set around the function, required for valid syntax, and a
+    // second empty set calls the surrounded function.
+    source = '(' + source + ')();'
+  }
+
+  // Create a script node holding this  source code.
+  var script = document.createElement('script');
+  script.setAttribute("type", "application/javascript");
+  script.textContent = source;
+
+  // Insert the script node into the page, so it will run, and immediately
+  // remove it to clean up.
+  document.body.appendChild(script);
+  document.body.removeChild(script);
+};
+
+// Inject contentScope()
+contentEval(contentScope);
